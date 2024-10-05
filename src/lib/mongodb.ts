568e2fa12859a7a970053243aa9c480e0e59d131
Héctor González <92290861+HectorGonzalezM@users.noreply.github.com>
2024-10-04 19:32:49 -0600

    Update mongodb.ts

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -9,23 +9,25 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
+// Declare the global variable using 'let' instead of 'var'
+declare global {
+  // Use 'let' to allow reassignment if needed
+  let _mongoClientPromise: Promise<MongoClient>;
+}
+
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
-
-declare global {
-  var _mongoClientPromise: Promise<MongoClient>;
-}
 
 if (process.env.NODE_ENV === 'development') {
   // Use a global variable in development to prevent creating multiple connections
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri as string, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production, create a new client
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri as string, options);
   clientPromise = client.connect();
 }
 
